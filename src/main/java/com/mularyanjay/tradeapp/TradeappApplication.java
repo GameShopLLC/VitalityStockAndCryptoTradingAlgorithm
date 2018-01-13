b437053485c16d949f39f7753e6e589cb015216f
Jay Evans <lyndenjayevans@gmail.com>
2018-01-13 17:32:18 -0500

    Scale down memory

--- src/main/java/com/mularyanjay/tradeapp/TradeappApplication.java
+++ src/main/java/com/mularyanjay/tradeapp/TradeappApplication.java
@@ -86,13 +86,13 @@ public class TradeappApplication extends SpringBootServletInitializer  {
 	//public TradeGroup(String whatName, int whatAmountThreads, BigDecimal initialUSD, int timeSpan, int ccn, float bto, float sto) {
 	@Bean
 	VitalityInstance vitalityInstance() {
-		return new VitalityInstance(new BigDecimal("300000"), 
-				new TradeGroup("One-1", 100, new BigDecimal("50000"), 1, 3, 8L * 60L * 1000L, 24L * 60L * 60L * 1000L),
-				new TradeGroup("Five-1", 100, new BigDecimal("50000"), 5, 3, 40L * 60L * 1000L, 24L * 60L * 60L * 1000L),
-				new TradeGroup("Ten-1", 100, new BigDecimal("50000"), 10, 3, 80L * 60L * 1000L, 24L * 60L * 60L * 1000L),
-				new TradeGroup("Fifteen-1", 100, new BigDecimal("50000"), 15, 3, 120L * 60L * 1000L, 24L * 60L * 60L * 1000L),
-				new TradeGroup("Thirty-1", 100, new BigDecimal("50000"), 30, 3, 240L * 60L * 1000L, 24L * 60L * 60L * 1000L),
-				new TradeGroup("Hour-1", 100, new BigDecimal("50000"), 60, 3, 480L * 60L * 1000L, 24L * 60L * 60L * 1000L));
+		return new VitalityInstance(new BigDecimal("75000"), 
+				new TradeGroup("One-1", 50, new BigDecimal("25000"), 1, 3, 8L * 60L * 1000L, 24L * 60L * 60L * 1000L),
+				new TradeGroup("Five-1", 20, new BigDecimal("10000"), 5, 3, 40L * 60L * 1000L, 24L * 60L * 60L * 1000L),
+				new TradeGroup("Ten-1", 20, new BigDecimal("10000"), 10, 3, 80L * 60L * 1000L, 24L * 60L * 60L * 1000L),
+				new TradeGroup("Fifteen-1", 20, new BigDecimal("10000"), 15, 3, 120L * 60L * 1000L, 24L * 60L * 60L * 1000L),
+				new TradeGroup("Thirty-1", 20, new BigDecimal("10000"), 30, 3, 240L * 60L * 1000L, 24L * 60L * 60L * 1000L),
+				new TradeGroup("Hour-1", 20, new BigDecimal("10000"), 60, 3, 480L * 60L * 1000L, 24L * 60L * 60L * 1000L));
 
 	}
 //	@Bean
