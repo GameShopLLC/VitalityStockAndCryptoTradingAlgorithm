42c5ba0c093159bc2b01f1b9612171308bb2293a
Jay Evans <lyndenjayevans@gmail.com>
2018-03-25 16:45:23 -0400

    show epoch time candle

--- src/main/resources/static/js/simmain.js
+++ src/main/resources/static/js/simmain.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	$('#getEpochTimeResult').click(function(){
 		$.ajax({
-			url:'https://ancient-crag-48261.herokuapp.com/startAlgorithm',
+			url:'https://ancient-crag-48261.herokuapp.com/showEpochTimeCandle',
 			type: 'POST',
 			data: {"from":$('#testEpochTime').val(),
 					"to":$('#testEpochTime').val()
