e4adfd53c7f55585a01adbfb9de4951a0d8be71f
Jay Evans <lyndenjayevans@gmail.com>
2018-03-25 16:50:49 -0400

    lol

--- src/main/resources/static/js/simmain.js
+++ src/main/resources/static/js/simmain.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
 	$('#getEpochTimeResult').click(function(){
 		$.ajax({
+			contentType:'application/json',
+			dataType:'application/json',
 			url:'https://ancient-crag-48261.herokuapp.com/showEpochTimeCandle',
 			type: 'POST',
 			data: {"from":$('#testEpochTime').val(),
