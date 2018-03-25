4a9ea4001e5808cc1b3d4ccc7ad3907f5ae2da37
Jay Evans <lyndenjayevans@gmail.com>
2018-03-25 16:36:35 -0400

    fixed typo

--- src/main/resources/static/js/simmain.js
+++ src/main/resources/static/js/simmain.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	$('#getEpochTimeResult').click(function(){
-		$.ajax1({
+		$.ajax({
 			url:'https://ancient-crag-48261.herokuapp.com/startAlgorithm',
 			type: 'POST',
 			data: {"from":$('#testEpochTime').val(),
