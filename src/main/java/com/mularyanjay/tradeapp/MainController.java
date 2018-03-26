7edf089e53130b015312158a7381f17fb3452ce7
Jay Evans <lyndenjayevans@gmail.com>
2018-03-25 20:59:53 -0400

    added

--- src/main/java/com/mularyanjay/tradeapp/MainController.java
+++ src/main/java/com/mularyanjay/tradeapp/MainController.java
@@ -452,17 +452,17 @@ public class MainController {
 	}
 	
 	@RequestMapping(value="/showEpochTimeCandle", method=RequestMethod.POST)
-	public @ResponseBody SerializableCandle showEpochTimeCandle() {//@RequestBody Map<String, String> map) {
+	public @ResponseBody String showEpochTimeCandle() {//@RequestBody Map<String, String> map) {
 		ObjectMapper objectMapper = new ObjectMapper();
 		RestTemplate restTemplate = new RestTemplate();
 
 		System.out.println("in");
 		String url = "https://api.gdax.com/products/LTC-USD/candles?" + "start=" + "1522019164" + "&end=" + "1522019224" + "&granularity=60";
-		ResponseEntity<SerializableCandle> response = null;
+		ResponseEntity<String> response = null;
 
 		System.out.println("mid");
 		try {
-		response = restTemplate.exchange(url, HttpMethod.GET, httpEntityBean.getEntityFromUrl(url), new ParameterizedTypeReference<SerializableCandle>(){});//restTemplate.exchange(requestEntity, responseType)//
+		response = restTemplate.exchange(url, HttpMethod.GET, httpEntityBean.getEntityFromUrl(url), new ParameterizedTypeReference<String>(){});//restTemplate.exchange(requestEntity, responseType)//
 
 		System.out.println("try");
 		} catch(Throwable t) {
