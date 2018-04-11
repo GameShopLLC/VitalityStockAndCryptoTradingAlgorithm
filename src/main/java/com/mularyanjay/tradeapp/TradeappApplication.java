0b54d418f1314e987c1cc28af2d5c272d287e4ab
Jay Evans <lyndenjayevans@gmail.com>
2018-04-10 22:15:33 -0400

    5 mil sim

--- src/main/java/com/mularyanjay/tradeapp/TradeappApplication.java
+++ src/main/java/com/mularyanjay/tradeapp/TradeappApplication.java
@@ -113,8 +113,8 @@ public class TradeappApplication extends SpringBootServletInitializer  {
 	//public TradeGroup(String whatName, int whatAmountThreads, BigDecimal initialUSD, int timeSpan, int ccn, float bto, float sto) {
 	@Bean
 	VitalityInstance vitalityInstance() {
-		return new VitalityInstance(SimulationMode.SIMULATION, new BigDecimal("1000000"), //Original 25000000
-				new TradeGroup(SimulationMode.SIMULATION, "One-1", "NONE", 3600, new BigDecimal("1000000"), 1, 3, 8L * 60L * 1000L, 24L * 60L * 60L * 1000L));//),
+		return new VitalityInstance(SimulationMode.SIMULATION, new BigDecimal("5000000"), //Original 25000000
+				new TradeGroup(SimulationMode.SIMULATION, "One-1", "NONE", 3600, new BigDecimal("5000000"), 1, 3, 8L * 60L * 1000L, 24L * 60L * 60L * 1000L));//),
 				//new TradeGroup("Five-1", 20, new BigDecimal("10000"), 5, 3, 40L * 60L * 1000L, 24L * 60L * 60L * 1000L),
 				//new TradeGroup("Ten-1", 20, new BigDecimal("10000"), 10, 3, 80L * 60L * 1000L, 24L * 60L * 60L * 1000L),
 				//new TradeGroup("Fifteen-1", 20, new BigDecimal("10000"), 15, 3, 120L * 60L * 1000L, 24L * 60L * 60L * 1000L),
