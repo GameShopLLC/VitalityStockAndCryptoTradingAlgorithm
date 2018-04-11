8d2cd409d9449e8b696888a8e46fa9d518e92a9f
Jay Evans <lyndenjayevans@gmail.com>
2018-04-11 15:51:26 -0400

    february test force loss

--- src/main/java/com/mularyanjay/tradeapp/SimulationManager.java
+++ src/main/java/com/mularyanjay/tradeapp/SimulationManager.java
@@ -84,8 +84,8 @@ public class SimulationManager {
 		
 		List<SerializableCandle> data = new ArrayList<SerializableCandle>();
 		HashMap<String, String> map = new HashMap<String, String>();
-		ComparableDateTime cdt = new ComparableDateTime("2018-03-1T00:00:00Z");
-		for (int i = 0; i < 192; i++) { //1882 for a year //273 for feb and march //144 march 129 feb
+		ComparableDateTime cdt = new ComparableDateTime("2018-01-1T00:00:00Z");
+		for (int i = 0; i < 132; i++) { //1882 for a year //273 for feb and march //144 march 129 feb
 			map.clear();
 			String from = cdt.toString();
 			cdt.incrementMinute(300);
