5e274316a497f2d04d500a770612374ba07cc5b6
Jay Evans <lyndenjayevans@gmail.com>
2018-04-13 15:51:39 -0400

    february 32 threads

--- src/main/java/com/mularyanjay/tradeapp/SimulationManager.java
+++ src/main/java/com/mularyanjay/tradeapp/SimulationManager.java
@@ -84,8 +84,8 @@ public class SimulationManager {
 		
 		List<SerializableCandle> data = new ArrayList<SerializableCandle>();
 		HashMap<String, String> map = new HashMap<String, String>();
-		ComparableDateTime cdt = new ComparableDateTime("2018-01-1T00:00:00Z");
-		for (int i = 0; i < 450; i++) { //1882 for a year //273 for feb and march //153 march 144 feb
+		ComparableDateTime cdt = new ComparableDateTime("2018-02-1T00:00:00Z");
+		for (int i = 0; i < 144; i++) { //1882 for a year //273 for feb and march //153 march 144 feb
 			map.clear();
 			String from = cdt.toString();
 			cdt.incrementMinute(300);
