e149b5b51e82f537687e50efdbf2eef6a838edaf
Jay Evans <lyndenjayevans@gmail.com>
2018-12-13 13:18:22 -0500

    confirm database creation

--- src/main/java/com/mularyanjay/tradeapp/DatabaseConfig.java
+++ src/main/java/com/mularyanjay/tradeapp/DatabaseConfig.java
@@ -1,5 +1,20 @@
 package com.mularyanjay.tradeapp;
 
-public class DatabaseConfig {
+import javax.sql.DataSource;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+    	System.out.println("Made database");
+        return DataSourceBuilder.create().build();
+    }
 }
