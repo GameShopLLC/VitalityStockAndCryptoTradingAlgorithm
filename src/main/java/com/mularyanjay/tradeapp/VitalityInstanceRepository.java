addc06a8c9fb3504e831e16391acbda1f811d5cd
Jay Evans <lyndenjayevans@gmail.com>
2018-12-14 16:28:05 -0500

    oh the repository

--- src/main/java/com/mularyanjay/tradeapp/VitalityInstanceRepository.java
+++ src/main/java/com/mularyanjay/tradeapp/VitalityInstanceRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.repository.MongoRepository;;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;;
 
+@Repository
 public interface VitalityInstanceRepository extends MongoRepository<VitalityInstance, String>  {
 
 	
