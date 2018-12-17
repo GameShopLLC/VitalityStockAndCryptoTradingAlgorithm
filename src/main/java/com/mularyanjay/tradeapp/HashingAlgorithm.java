85cbfcf032e4d7cc2215e0f7bc4daa3b70761554
Jay Evans <lyndenjayevans@gmail.com>
2018-12-16 21:29:38 -0500

    /orders

--- src/main/java/com/mularyanjay/tradeapp/HashingAlgorithm.java
+++ src/main/java/com/mularyanjay/tradeapp/HashingAlgorithm.java
@@ -68,7 +68,7 @@ public class HashingAlgorithm {
 		timestamp = Instant.now().getEpochSecond() + "";
 		Mac sha256 = null;
 		try {
-			String prehash = timestamp + "POST".toUpperCase() + requestPath + body; 
+			String prehash = timestamp + "POST".toUpperCase() + "/orders" + body; 
 			//+ "";
 			System.out.println(prehash);
 			byte[] secretDecoded = Base64.getDecoder().decode(secret);
