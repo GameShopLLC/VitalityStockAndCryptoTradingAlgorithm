4fe8054b7031fec581d5be6daf5c1532a4f8de6f
Jay Evans <lyndenjayevans@gmail.com>
2018-12-24 00:03:28 -0500

    fix and buildpack

---
+++ src/main/java/com/mularyanjay/tradeapp/FixEngine.java
@@ -0,0 +1,15 @@
+package com.mularyanjay.tradeapp;
+
+public class FixEngine implements Runnable {
+
+	public FixEngine() {
+		
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
